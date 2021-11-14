fe66abd904052281cc270aafba6fd01e6fa3a41c
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2021-11-14 17:05:08 +0300

    При отрицательном числе и числе, в случае которого получается дробный результат - выводится 0

--- src/main/java/Lesson4/FourthLesson.java
+++ src/main/java/Lesson4/FourthLesson.java
@@ -3,17 +3,21 @@ package Lesson4;
 public class FourthLesson {
 
     public static void main(String[] args) {
-        System.out.println(squareRoot(64));
+        System.out.println(squareRoot(121));
     }
 
     public static int squareRoot(int num) {
         int degree = 1;
         if (num < 2) {
-            return num;
+            return 0;
         }
         while (degree * degree <= num) {
+            if(degree * degree == num){
+                return degree;
+            }
             degree++;
         }
-        return --degree;
+
+        return 0;
     }
 }
