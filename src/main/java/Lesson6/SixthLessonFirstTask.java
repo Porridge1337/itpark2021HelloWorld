e819aa5f21be0d0d585a20023faef643326f2d57
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2021-11-22 23:19:48 +0300

    Выполнение задачи по вычислению max, min, avg и вычисление ряда Фибоначчи, используя массив как промежуточный кэш.

---
+++ src/main/java/Lesson6/SixthLessonFirstTask.java
@@ -0,0 +1,60 @@
+package Lesson6;
+
+import java.util.Scanner;
+
+public class SixthLessonFirstTask {
+    public static void main(String[] args) {
+        double[] numbers = fillArray(printNumber());
+        System.out.println("--------------------------");
+        System.out.println(maxNumber(numbers) + " - максимальное значение");
+        System.out.println(minNumber(numbers) + " - минимальное значение");
+        System.out.println(average(numbers) + " - среднее значение");
+
+    }
+
+    public static double maxNumber(double[] array) {
+        double num = array[0];
+
+        for (int i = 0; i <= array.length - 1; i++) {
+            if (num < array[i]) num = array[i];
+        }
+        return num;
+    }
+
+    public static double minNumber(double[] array) {
+        double num = array[0];
+
+        for (int i = 0; i <= array.length - 1; i++) {
+            if (num > array[i]) num = array[i];
+        }
+        return num;
+    }
+
+    public static double average(double[] array) {
+        double num = 0;
+        for (int i = 0; i <= array.length - 1; i++) {
+            num += array[i];
+        }
+        return num / array.length;
+    }
+
+    public static int printNumber() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Укажите размер массива: ");
+        while (!sc.hasNextInt()) {
+            String error = sc.next();
+            System.out.println("Ошибка, введено: " + error);
+            System.out.print("Введите пожалуйста число: ");
+        }
+        return sc.nextInt();
+    }
+
+    public static double[] fillArray(int size) {
+        double numbers[] = new double[size];
+        for (int i = 0; i <= numbers.length - 1; i++) {
+            numbers[i] = Math.round(Math.random() * 1000.0) / 1000.0;
+            System.out.println(numbers[i]);
+        }
+        return numbers;
+    }
+}
---
+++ src/main/java/Lesson6/SixthLessonSecondTask.java
@@ -0,0 +1,73 @@
+package Lesson6;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SixthLessonSecondTask {
+
+    public static void main(String[] args) {
+        int[] cache = new int[10];
+        String proceed = "y";
+        while (proceed.equals("y")) {
+            int num = printNumber() - 1;
+            cache = isExceed(cache, num + 1);
+            if (getArrayIndex(cache, num) != num) {
+                putInCash(cache, num, recursionFibonacci(num));
+            }
+            System.out.println("Результат = " + cache[num]);
+            System.out.println("Если хотите продолжить - нажмите 'y', если остановиться - любую другую кнопку");
+            proceed = printLetter();
+        }
+        System.out.println("Программа закончила выполнение");
+    }
+
+    public static int recursionFibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return recursionFibonacci(n - 1) + recursionFibonacci(n - 2);
+        }
+    }
+
+    public static int[] isExceed(int[] cache, int index) {
+        if (cache.length < index) {
+            cache = resizeCache(cache, index);
+        }
+        return cache;
+    }
+
+    public static int[] resizeCache(int[] cache, int newSize) {
+        return Arrays.copyOf(cache, newSize);
+    }
+
+    public static void putInCash(int cache[], int index, int num) {
+        cache[index] = num;
+    }
+
+    public static int getArrayIndex(int[] array, int number) {
+        for (int i = 0; i <= array.length - 1; i++) {
+            if (array[i] == array[number]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int printNumber() {
+        System.out.print("Введите число: ");
+        Scanner sc = new Scanner(System.in);
+        while (!sc.hasNextInt()) {
+            String error = sc.next();
+            System.out.println("Ошибка, введено: " + error);
+            System.out.print("Введите пожалуйста число: ");
+        }
+        return sc.nextInt();
+    }
+
+    public static String printLetter() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+}
