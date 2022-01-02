9f66d5ce3c229fad2234f3c5cbc0b54bc821237b
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-01-02 17:29:56 +0300

    Мелкие правки в 16-м задании

--- src/main/java/lesson16/LoggerRunner.java
+++ src/main/java/lesson16/LoggerRunner.java
@@ -10,11 +10,11 @@ public class LoggerRunner {
         AtomicBoolean flag1 = new AtomicBoolean(false);
         AtomicBoolean flag2 = new AtomicBoolean(false);
         AtomicBoolean flag3 = new AtomicBoolean(false);
-        Logger log1 = new Logger("LogThread-1",flag1, 3); //sleepTime указывать в секундах
+        Logger log1 = new Logger("LogThread-1", flag1, 3); //sleepTime указывать в секундах
+        Logger log2 = new Logger("LogThread-2", flag2, 2);
+        Logger log3 = new Logger("LogThread-3", flag3, 4);
         log1.start();
-        Logger log2 = new Logger("LogThread-2",flag2, 2);
         log2.start();
-        Logger log3 = new Logger("LogThread-3",flag3, 4);
         log3.start();
         Thread.sleep(60 * 1000);
         flag1.set(true);
