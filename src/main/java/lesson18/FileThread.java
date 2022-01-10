cc030ffa85f635369adddfbe37b5c28ddd536b79
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-01-10 15:10:09 +0300

    Правка по code convection статической переменной READ_LOCATION, изменил способ записи файлов с

--- src/main/java/lesson18/FileThread.java
+++ src/main/java/lesson18/FileThread.java
@@ -1,11 +1,13 @@
 package lesson18;
 
+import java.io.File;
+
 public class FileThread implements Runnable {
 
-    private static final String readLocation = "./src/main/resources/lesson18/fileRead/text.txt";
+    private static final String READ_LOCATION = "./src/main/resources/lesson18/fileRead/text.txt";
 
     @Override
     public void run() {
-        new FileWriterAndReader().readFile(readLocation);
+        new FileWriterAndReader().readFile(new File(READ_LOCATION));
     }
 }
--- src/main/java/lesson18/FileWriterAndReader.java
+++ src/main/java/lesson18/FileWriterAndReader.java
@@ -1,20 +1,16 @@
 package lesson18;
 
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.FileWriter;
+import java.io.*;
 
 public class FileWriterAndReader {
 
-    public void readFile(String readLocation) {
+    public void readFile(File readLocation) {
         try (BufferedReader br = new BufferedReader(new FileReader(readLocation))) {
             String text = "";
             int i = 0;
             while ((text = br.readLine()) != null) {
-                writeFile("./src/main/resources/lesson18/fileWrite/" + ++i + ".txt", text);
+                writeFile(readLocation.getParent() + "/fileWrite/" + ++i + ".txt", text);
             }
         } catch (IOException ex) {
             ex.printStackTrace();
