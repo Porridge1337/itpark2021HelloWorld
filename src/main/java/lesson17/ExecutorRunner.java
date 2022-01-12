57a606415115f7627d8c15ec6ad12a433b0286b5
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-01-12 16:21:51 +0300

    Выполнение 17-го задания

---
+++ src/main/java/lesson17/ExecutorRunner.java
@@ -0,0 +1,49 @@
+package lesson17;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+public class ExecutorRunner {
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 1_000_000; i++) {
+            numbers.add(new Random().nextInt(1_000_000_000));
+        }
+        Collections.sort(numbers);
+
+        Callable<Integer> min = () -> {
+            System.out.println("Минимальное значение = " + numbers.get(0));
+            return numbers.get(0);
+        };
+        Callable<Integer> average = () -> {
+            int temp = 0;
+            for (int i = 0; i < numbers.size(); i++) {
+                temp += numbers.get(i);
+            }
+            temp = temp / numbers.size();
+            System.out.println("Среднее значение = " + temp);
+            return temp;
+        };
+        Callable<Integer> max = () -> {
+            System.out.println("Максимальное значение = " + numbers.get(numbers.size() - 1));
+            return numbers.get(numbers.size() - 1);
+        };
+
+        List<Callable<Integer>> threads = new ArrayList<>();
+        threads.add(min);
+        threads.add(average);
+        threads.add(max);
+
+        executorService.invokeAll(threads);
+        executorService.shutdown();
+
+    }
+}
